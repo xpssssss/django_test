23effaa72bff0c85733653b8334fa406e9a5457b
xpssssss <handongping020820@126.com>
2024-01-20 12:00:47 +0800

    我的第一个版本练习

---
+++ inventory_management_django_system-master/后台管理系统联系模板/README.md
@@ -0,0 +1,39 @@
+# inventory_management_django_system
+
+#### 介绍
+数据库大作业——前后端分离的后台管理系统——django后端部分
+
+#### 软件架构
+本项目是一个前后端分离的后台管理系统，前端采用Vue+Element-Plus+Echarts，后端采用Django+PostgreSQL+Redis。
+
+● 本系统为一个简单的后台管理系统，可为企业提供后台管理服务，主要包括系统用户的登录注册、库存管理、订单管理、临时订单、用户管理、数据可视化六大板块的功能。
+
+● 其中系统用户分为两类，一类是高权限的超级管理员，另一类是低权限的普通管理员，其中普通管理员不具备用户管理功能。系统用户既是系统的管理员，也是订单的负责人，每个管理员负责自己的订单与临时订单。
+
+● 在注册页面默认注册低权限的普通管理员，超级管理员可在用户管理板块中对系统的用户权限进行修改。
+
+● 每个订单/临时订单会通过userId这个属性与用户绑定，类似于公司中某个订单是谁负责的，增加订单/临时订单时通过验证目前所登录信息来做到绑定。
+
+● 数据可视化模块中，主要对公司近十年具体的销售额数据进行可视化，里面的数据通过sql语句初始化在数据库表里面，前端无需提供，后端也无法更改，相当于一个独立静态的模块，无数据的改变。
+
+前端代码链接：https://gitee.com/fish-in-jiangan-river/inventory_management_system
+
+（看官别忘记给个star哦！）
+
+
+#### 安装教程
+
+`git clone https://gitee.com/fish-in-jiangan-river/inventory_management_system.git`
+
+#### 使用说明
+
+`python .\manage.py runserver`
+
+#### 特技
+
+1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
+2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
+3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
+4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
+5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
+6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
---
+++ inventory_management_django_system-master/后台管理系统联系模板/inventory_management_django_system/urls.py
@@ -0,0 +1,91 @@
+"""inventory_management_django_system URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# 导入所需模块
+from django.urls import path
+from app import views_auth, views_ware, views_user, views_sales, views_order, views_cart
+from django.views.generic import TemplateView
+
+# 注册路由
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
+
+    # 库存管理模块--miserware
+    # 新增库存
+    path("miserware/addware", views_ware.addware),
+    # 删除库存
+    path("miserware/deleteWare", views_ware.deleteWare),
+    # 更改库存
+    path("miserware/updateWare", views_ware.updateWare),
+    # 获取物品清单
+    path("miserware/getWarelist", views_ware.getWarelist),
+    # 获取单个物品信息
+    path("miserware/getWare/<theme>", views_ware.getWare, name='wareid'),
+
+
+    # 用户管理模块--miseruser
+    # 增加用户
+    path("miseruser/addUser", views_user.addUser),
+    # 删除用户
+    path("miseruser/deleteUser", views_user.deleteUser),
+    # 更改用户
+    path("miseruser/updateUser", views_user.updateUser),
+    # 获取用户信息清单
+    path("miseruser/getUserlist", views_user.getUserlist),
+    # 获取单个用户信息
+    path("miseruser/getUser/<theme>", views_user.getUser, name='userid'),
+
+
+    # 订单管理模块--miserorder
+    # 添加订单
+    path("miserorder/addOrder", views_order.addOrder),
+    # 删除订单
+    path("miserorder/deleteOrder", views_order.deleteOrder),
+    # 查询单个订单
+    path("miserorder/getOrder/<theme>", views_order.getOrder, name='orderid'),
+    # 获取全部订单
+    path("miserorder/getOrderlist", views_order.getOrderlist),
+
+
+    # 临时订单模块--misercart
+    # 添加临时订单
+    path("misercart/addCart", views_cart.addCart),
+    # 删除临时订单
+    path("misercart/deleteCart", views_cart.deleteCart),
+    # 编辑临时订单
+    path("misercart/updateCart", views_cart.updateCart),
+    # 查询临时订单,需要判断用户权限，高于100才能获取全部
+    path("misercart/getCartlist", views_cart.getCartlist),
+    # 提交临时订单
+    path("misercart/transCart", views_cart.transCart),
+
+
+    # 登录注册模块--miserauth
+    # 注册功能
+    path("miserauth/register", views_auth.userRegister),
+    # 登录功能
+    path("miserauth/login", views_auth.userLogin),
+    # 获取当前登录信息
+    path("miserauth/getloginUser", views_auth.getLoginUser),
+    # 退出登录
+    path("miserauth/loginout", views_auth.loginOut),
+
+
+    # 数据可视化模块--misersales
+    # 将销售数据传入数据库
+    path("misersales/saveSalesData", views_sales.saveSalesData),
+    # 获取数据库中的销售数据
+    path("misersales/getSalesData", views_sales.getSalesData)
+]
